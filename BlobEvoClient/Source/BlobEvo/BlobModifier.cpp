8bb7418ab4b9541c92b4c691283c3b1a8d1740bd
OmerWexler <omerwexler14@gmail.com>
2020-05-11 01:31:10 +0300

    - Added donut model as blob food. - Made a cpp client to python server socket connection.
    Signed-off-by: OmerWexler <omerwexler14@gmail.com>

---
+++ BlobEvoClient/Source/BlobEvo/BlobModifier.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlobModifier.h"
+
+BlobModifier::BlobModifier()
+{
+}
+
+BlobModifier::~BlobModifier()
+{
+}
